1e913dd72f13c9dd0b58ad92308bd1016c571330
SimmiCodes <96693326+SimmiCodes@users.noreply.github.com>
2023-09-29 21:32:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+    event.preventDefault();
+    var radiusInput = document.getElementById("radius");
+    var volumeOutput = document.getElementById("volume");
+    var radius = parseFloat(radiusInput.value);
+    if (isNaN(radius)) {
+        volumeOutput.value = "Invalid input";
+    } else {
+        var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        volumeOutput.value = volume.toFixed(2);
+    }
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+window.onload = function() {
+    var form = document.getElementById('MyForm');
+    form.onsubmit = volume_sphere;
+};
